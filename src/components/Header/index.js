73f9a77dbfb915ac85674232733adfe8715de2df
Fernando Angulo Donoso <fangulo82@gmail.com>
2020-12-21 20:21:09 -0800

    Adding index.js for the heather

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,1 +1,11 @@
+import "./style.css";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <h1>Employee Directory</h1>
+      <p>Click on carrots to filter by heading or use the search box to narrow your results.</p>
+    </div>
+  )
+}
 
