03d7315c8c1dc1682762fb5ddc8462d2b2b1ee11
Fernando Angulo Donoso <fangulo82@gmail.com>
2020-12-22 20:15:21 -0800

    removing some text from the header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,7 +4,6 @@ export default function Header() {
   return (
     <div className="header">
       <h1>Employee Directory</h1>
-      <p>Click on carrots to filter by heading or use the search box to narrow your results.</p>
     </div>
   )
 }
